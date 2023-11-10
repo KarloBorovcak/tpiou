70818003f28ac43d97afd651ebb509d850f139b7
Karlo Borovcak <borovcak.karlo@gmail.com>
2023-11-10 16:10:28 +0100

    pylint fix

--- lab1/consumer/consumer.py
+++ lab1/consumer/consumer.py
@@ -6,11 +6,21 @@ EVENT_HUB_NAME = os.getenv("EVENT_HUB_NAME")
 CONSUMER_GROUP = "$Default"
 
 def on_event(partition_context, event):
+    """
+    Callback function for receiving events.
+
+    Args:
+        partition_context (azure.eventhub.EventContext): The context information for the partition.
+        event (azure.eventhub.EventData): The received event.
+    """
     context = partition_context
-    print("Received event from partition: {}".format(context.partition_id))
+    print(f"Received event from partition: {context.partition_id}")
     print(event.body_as_str(encoding='UTF-8'))
 
 def receive():
+    """
+    Start receiving events from the Event Hub.
+    """
     # Create a consumer client for the event hub.
     client = EventHubConsumerClient.from_connection_string(
         conn_str=EVENT_HUB_CONNECTION_STR,
